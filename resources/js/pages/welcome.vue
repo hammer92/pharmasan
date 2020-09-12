c0a2f8b1d679325a881cecf0f643f5fbb8c20fd8
Hammer Pinto <jjpb92@gmail.com>
2020-09-12 02:44:34 -0500

    ultimos cambios

--- resources/js/pages/welcome.vue
+++ resources/js/pages/welcome.vue
@@ -22,7 +22,7 @@
       </div>
 
       <div class="links">
-        <a href="https://github.com/cretueusebiu/laravel-vue-spa">github.com/cretueusebiu/laravel-vue-spa</a>
+        <a href="https://github.com/hammer92/pharmasan.git">github.com/hammer92/pharmasan.git</a>
       </div>
     </div>
   </div>
